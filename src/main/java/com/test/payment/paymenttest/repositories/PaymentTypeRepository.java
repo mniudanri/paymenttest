6dee4ba5640fbc14babc18425e81d972ea04230e
mniudanri <niudanri12@gmail.com>
2023-08-28 21:38:19 +0700

    remove unused tag

--- src/main/java/com/test/payment/paymenttest/repositories/PaymentTypeRepository.java
+++ src/main/java/com/test/payment/paymenttest/repositories/PaymentTypeRepository.java
@@ -1,11 +1,9 @@
 package com.test.payment.paymenttest.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.test.payment.paymenttest.entities.PaymentType;
 
-@Repository
 public interface PaymentTypeRepository extends JpaRepository<PaymentType, Long> {
     
 }
