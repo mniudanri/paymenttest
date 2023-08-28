cda45349f3631d67d1c9516e36d11d80d5ac1260
mniudanri <niudanri12@gmail.com>
2023-08-28 18:27:05 +0700

    add query records

---
+++ src/main/query.sql
@@ -0,0 +1,19 @@
+CREATE TABLE payment (
+	"payment_id" SERIAL PRIMARY KEY,
+	"amount" INTEGER NOT NULL,
+	"payment_type_id" BIGINT NOT NULL,
+	"date" TIMESTAMPTZ DEFAULT now(),
+	"customer_id" BIGINT NOT NULL
+);
+
+CREATE TABLE payment_type (
+	"payment_type_id" SERIAL PRIMARY KEY,
+	"type_name" VARCHAR NOT NULL
+);
+
+CREATE TABLE inventory (
+	"item_id" SERIAL PRIMARY KEY,
+	"item_name" VARCHAR NOT NULL,
+	"quantity" INTEGER NOT NULL,
+	"price" INTEGER NOT NULL
+);
