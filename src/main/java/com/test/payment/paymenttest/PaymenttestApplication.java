3a1846d0243b6920a0e52b08425a7652a1b3ca6c
mniudanri <niudanri12@gmail.com>
2023-08-28 10:26:43 +0700

    init project

---
+++ src/main/java/com/test/payment/paymenttest/PaymenttestApplication.java
@@ -0,0 +1,13 @@
+package com.test.payment.paymenttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymenttestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymenttestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/payment/paymenttest/PaymenttestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.payment.paymenttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymenttestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
